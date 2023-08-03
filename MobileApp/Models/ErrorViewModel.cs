189c6228f1f145f7825bc04e9afb615366bfc3de
kieul <kieulslz123@gmail.com>
2023-08-03 13:20:52 +0700

    Add project files.

---
+++ MobileApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
